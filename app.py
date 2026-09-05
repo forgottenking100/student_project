928c93e764432b0d1855df589181699f9104c612
forgottenking100 <forgottenking100@gmail.com>
2026-09-05 23:44:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
 import streamlit as kd
-kd.title("welcome to life of kavin")
+kd.title("Login Page")
+username = kd.text_input("Username")
+password = kd.text_input("Password", type="password")
+valid_username = "kavin"
+valid_password = "kavin@123"
+if kd.button("Login"):
+    if username == valid_username and password == valid_password:
+        kd.success(" Login successful! Welcome, " + username)
+    else:
+        kd.error("Invalid username or password")
